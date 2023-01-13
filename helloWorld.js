00cd6d8fffb03a043d667766db7562b25cb04f62
Tracey <tturner.zn@gmail.com>
2023-01-13 16:48:51 +0200

    Git Basics

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+// Declare function
+function say(something) {
+  console.log(something);
+}
+
+// Call function
+say("Git is awesome!");
+ 
+//This site helped https://makersaid.com/how-to-run-javascript-on-a-mac/
